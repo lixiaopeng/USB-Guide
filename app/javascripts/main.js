17a0655dec1dcd8e5189bd9c8afe251805b25008
Wangye Zhao <ivanzhaowy@gmail.com>
2013-09-10 19:46:12 +0800

    Add error UI

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -94,6 +94,7 @@ require([
     };
 
     $(function () {
+        show('error-debug');
         window.external.call('ready');
     });
 });
--- app/javascripts/nls/en/lang.js
+++ app/javascripts/nls/en/lang.js
@@ -5,5 +5,12 @@ define({
     SPEED_TIP : '{1}% &bull; {2}/s',
     INSTALLING_TIP : '安装过程中不要重起电脑，请耐心等候',
     CREDIT_TIP : '请按照提示点击「始终安装此驱动软件程序」保证驱动正确安装',
-    CREDIT_FALLBACK_TIP : '电脑上没出现这个提示'
+    CREDIT_FALLBACK_TIP : '电脑上没出现这个提示',
+    RESET_USB : '重新插拔 USB 线',
+    RESTART_PHONE : '重新启动手机',
+    RESTART_PC : '重新启动豌豆荚或电脑',
+    ERROR_STORAGE : '豌豆荚在您的手机上安装失败，可能是由于您的手机内存存储空间不足。安装豌豆荚需要大概 10M 的空间，请您清理手机垃圾，或者卸载一些软件后重试。',
+    RETRY : '重试',
+    USB_DEBUG : '如果您手机是 Android 4.2 版本以上的操作系统，请在手机的提示中允许「USB 调试」',
+    USB_FALLBACK_TIP : '手机上没有这个提示，试试别的办法'
 });
