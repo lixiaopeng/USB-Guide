0093d920cb3c7a0673f7889192aa4c985f213e0d
Wangye Zhao <ivanzhaowy@gmail.com>
2013-09-10 19:58:28 +0800

    Bug fix

--- app/javascripts/main.js
+++ app/javascripts/main.js
@@ -45,19 +45,19 @@ require([
             }
         }
         if (id == "connecting-start") {
-            if (data.screen_name != "") {
+            if (data.screen_name !== "") {
                 $(".g-tips.h5").html(FormatString(lang.CONNECTION_START, data.screen_name));
-            } else if (data.brand != "") {
+            } else if (data.brand !== "") {
                 $(".g-tips.h5").html(FormatString(lang.CONNECTION_START, data.brand + "手机"));
             } else {
                 $(".g-tips.h5").html(FormatString(lang.CONNECTION_START, "手机"));
             }
         }
-        if (id == "downloading") {
+        if (id === "downloading") {
             var progress = data.progress + "%";
             $(".g-progress .inner").width(progress);
             $(".g-tips.h5").html(FormatString(lang.DOWNLOADING_TIP));
-            
+
             var speedKbps = (data.speed / 1024).toFixed(2);
             var speedMbps = (speedKbps / 1024).toFixed(2);
             if (speedMbps >= 1) {
@@ -66,19 +66,19 @@ require([
                 $(".g-tips.h6").html(FormatString(lang.SPEED_TIP, data.progress, speedKbps + " KB"));
             }
         }
-        if (id == "installing") {
+        if (id === "installing") {
             $(".g-tips.h5").html(FormatString(lang.INSTALLING_TIP));
         }
-        if (id == "offline") {
+        if (id === "offline") {
             // TODO
         }
-        if (id == "storage_insufficient") {
+        if (id === "storage_insufficient") {
             // TODO
         }
-        if (id == "adbdebug_close") {
+        if (id === "adbdebug_close") {
             // TODO
         }
-        if (id == "connecting-error") {
+        if (id === "connecting-error") {
             // TODO
         }
     };
