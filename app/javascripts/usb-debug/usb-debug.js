a254d31d66c6bdcc641dcfe463a11b14a545bc92
lixiaopeng <lixiaopeng@wandoujia.com>
2013-12-23 14:58:56 +0800

    [Bugfix] encode device_key

--- app/javascripts/usb-debug/usb-debug.js
+++ app/javascripts/usb-debug/usb-debug.js
@@ -84,7 +84,7 @@
         };
 
         var sc = document.createElement('script');
-        var url = 'http://vmap.wandoujia.com:8888/query?callback=vmapCallBack&data=' +  encodeURIComponent(vid_pid) + '&device_key=' + device_key;
+        var url = 'http://vmap.wandoujia.com:8888/query?callback=vmapCallBack&data=' +  encodeURIComponent(vid_pid) + '&device_key=' + encodeURIComponent(device_key);
 
         //var data = window.external.call('{"cmd":"getData", "param":' + vid_pid + '}')
         //var url = 'http://vmap.wandoujia.com/query?callback=vmapCallBack&data=' +  data;
